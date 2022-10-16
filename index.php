47a0aba3a4968c6b7a19139cb3f46c262b3ab474
kwingo <50303717+kwingo@users.noreply.github.com>
2022-10-16 19:24:10 -0400

    Update index.php
    Adding get and delete endpoints.

--- index.php
+++ index.php
@@ -26,4 +26,35 @@ $app -> get('/inventory', function($request, $response, $args){
     return $response->withStatus(200)->withJson($payload);
 });
 
+//retrieving a single inventory
+$app->get('/inventory/{id}', function (Request $request, Response $response,  array $args) {
+
+    $id = $args['id'];
+
+    $inventory = new Inventory();
+    $_inv = $inventory->find($id);
+
+    $payload[$_inv->id] = [
+        'productID' => $_inv->productID,
+        'brandID' => $_inv->brandID,
+        'frameName' => $_inv->frameName,
+        'price' => $_inv->price,
+        'source' => $_inv->source
+        ];
+
+    return $response->withStatus(200)->withJson($payload);
+});
+
+//deleting a single inventory
+$app->delete('/inventory/{id}', function ($request, $response, $args) {
+    $id = $args['id'];
+    $_inv = Inventory::find($id);
+    $_inv->detele();
+    if ($_inv->exists) {
+        return $response->withStatus(200);
+    } else {
+        return $_inv->withStatus(204)->getBody()->write("Inventory '/inventory/$id' has been deleted.");
+    }
+});
+
 $app->run();
